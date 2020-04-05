0941b84695d8beb700d193328f59572d8650905e
juliuszCezar <michal.adam.cichy@gmail.com>
2020-04-05 20:01:15 +0200

    quads :)

--- gilgamesh/classes/Primitives.h
+++ gilgamesh/classes/Primitives.h
@@ -9,9 +9,10 @@
 class Primitives {
 private:
 	static Mesh* cube;
+	static Mesh* quad;
 public:
 	static void init() {
-		vector <Vertex> vertices = {
+		cube = new Mesh(vector <Vertex> {
 			Vertex(vec3(-0.5f, -0.5f, -0.5f),  vec2(0.0f,  0.0f), vec3(0.0f,  0.0f, -1.0f), 0.0f),
 			Vertex(vec3(0.5f, -0.5f, -0.5f),  vec2(1.0f,  0.0f), vec3(0.0f,  0.0f, -1.0f), 0.0f),
 			Vertex(vec3(0.5f,  0.5f, -0.5f),  vec2(1.0f,  1.0f), vec3(0.0f,  0.0f, -1.0f), 0.0f),
@@ -53,19 +54,33 @@ public:
 			Vertex(vec3(0.5f,  0.5f,  0.5f),  vec2(1.0f,  0.0f), vec3(0.0f,  1.0f,  0.0f), 0.0f),
 			Vertex(vec3(-0.5f,  0.5f,  0.5f),  vec2(0.0f,  0.0f), vec3(0.0f,  1.0f,  0.0f), 0.0f),
 			Vertex(vec3(-0.5f,  0.5f, -0.5f),  vec2(0.0f,  1.0f), vec3(0.0f,  1.0f,  0.0f), 0.0f)
-		};
-
-		vector <uint> indices{
+		},
+		vector <uint> {
 			0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35 //ALERT
-		};
+		}, TEXTURE_FIRE);
 
-		cube = new Mesh(vertices, indices, TEXTURE_FIRE);
+		quad = new Mesh(
+			vector <Vertex> {
+				Vertex(vec3(0.5f, 0.5f, 0.0f), vec2(1.0f, 1.0f), vec3(0, 0, 1), 0),
+				Vertex(vec3(0.5f, -0.5f, 0.0f), vec2(1.0f, 0.0f), vec3(0, 0, 1), 0),
+				Vertex(vec3(-0.5f, -0.5f, 0.0f), vec2(0.0f, 0.0f), vec3(0, 0, 1), 0),
+				Vertex(vec3(-0.5f, 0.5f, 0.0f), vec2(0.0f, 1.0f), vec3(0, 0, 1), 0)
+			},
+			vector <unsigned int> {
+				0, 1, 3,
+				1, 2, 3
+			}, TEXTURE_FIRE);
 	}
 
 	static Mesh* getCube() {
 		return cube;
 	}
+
+	static Mesh* getQuad() {
+		return quad;
+	}
 };
 
 
 Mesh* Primitives::cube;
+Mesh* Primitives::quad;
