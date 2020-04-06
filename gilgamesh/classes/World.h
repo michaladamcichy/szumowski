f358308d4d514fb9878e9fdf0210150fe3f38414
juliuszCezar <michal.adam.cichy@gmail.com>
2020-04-06 03:00:42 +0200

    done :)

--- gilgamesh/classes/World.h
+++ gilgamesh/classes/World.h
@@ -25,21 +25,26 @@ public:
 	World() {
 		globalCamera = Camera(vec3(0, 10, 10));
 
-		int rows = 100;
-		int columns = 100;
+		int rows = 20;
+		int columns = 20;
 
-		float step = 2.0;
+		float step = 40.0;
 
 		float distance = rows * step;
 
 		for (int r = 0; r < rows; r++) {
 			for (int c = 0; c < columns; c++) {
-				Virus* virus= new Virus(vec3(r * step - distance/2, 2, c * step - distance/2));
-				viruses.push_back(virus);
+				GameObject* object = new GameObject(Primitives::getCube(), TEXTURE_GROUND, vec3(r * step - distance/2, 20.0, c * step - distance/2), Config::get(DEFAULT_BUILDING_DIMENSIONS));
+				objects.push_back(object);
 			}
 		}
 
-		light.init(vec3(0, 10, 0), Directions::DOWN, Colors::WHITE, 0.1, 0.9, 0.0);
+		GameObject* ground = new GameObject(Primitives::getQuad(), TEXTURE_FIRE, vec3(0,0,0), Config::get(GROUND_DIMENSIONS));
+		ground->rotatePitch(Constants::HALF_PI);
+
+		objects.push_back(ground);
+
+		light.init(vec3(0, 10, 0), Directions::DOWN, Colors::WHITE, 0.5, 0.9, 0.0);
 	}
 
 	void handleInput(Mouse& mouse, Keyboard& keyboard) {
